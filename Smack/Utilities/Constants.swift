185bbac1aab2eafc399561b462a3d7c28a0edb27
Yaroslav Satsyuk <yaroslavsatsyuk@gmail.com>
2017-11-10 14:05:53 +0200

    create new file AuthServices add alamofire request for logging

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,9 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+
+let BASE_URL = "https://slackchattychatchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 
 //Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNEL = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
